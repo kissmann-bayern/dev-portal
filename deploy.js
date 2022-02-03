8d14f10624c75175983c31b9a7ccb8db0c02eff6
Christoph Kissmann <kissmannchristoph@gmail.com>
2022-02-03 20:49:16 +0100

    init

---
+++ deploy.js
@@ -0,0 +1,37 @@
+const { exec } = require("child_process");
+
+exec("git add index.html", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec('git commit -m "deploy"', (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("git checkout -B _deploy && git push heroku integration", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
