c313f84dd8e09a27a5d51a2b21f943a0116a453a
Christoph Kissmann <kissmannchristoph@gmail.com>
2022-02-03 22:51:50 +0100

    a

--- deploy.js
+++ deploy.js
@@ -1,5 +1,16 @@
 const { exec } = require("child_process");
 
+exec('mkdir .ssh && cd .ssh && touch config && touch privKey && echo heroku config:get PRIV_KEY > privKey && echo "Host github.com\n  HostName github.com\n IdentityFile ~/.ssh/privKey"', (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
 exec("git add index.html", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
